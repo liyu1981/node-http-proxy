daf974561d697b9c8b1f58cff0a5764c8efa5f67
root <root@realone-hue-1385696000867.(none)>
2013-12-03 13:45:21 +0800

    make rewrite rule more correct

--- bin/myroute.js
+++ bin/myroute.js
@@ -2,7 +2,7 @@ var _proxyServer = null;
 
 var huePattern = /^\//i;
 function hueServer(req, res, next) {
-  _proxyServer.web(req, res, { target: 'http://localhost:8888' });
+  _proxyServer.web(req, res, { target: 'http://localhost:8888', });
 }
 
 var hdfsPattern = /^\/hdfs/i;
@@ -10,7 +10,10 @@ function hdfsServer(req, res, next) {
   _proxyServer.web(req, res, { 
     target: 'http://localhost:50070',
     pathRewrite: function(path) {
-      return path.substring(5) || '/';
+      if (path.indexOf('/hdfs') === 0) {
+        return path.substring(5) || '/';
+      }
+      return path;
     } 
   });
 }
@@ -20,13 +23,17 @@ function mapredServer(req, res, next) {
   _proxyServer.web(req, res, { 
     target: 'http://localhost:50030',
     pathRewrite: function(path) {
-      return path.substring(7) || '/';
+      if (path.indexOf('/mapred') === 0) {
+        return path.substring(7) || '/';
+      }
+      return path;
     } 
   });
 }
 
 exports.register = function(server, proxyServer) {
   _proxyServer = proxyServer;
+  
   server.get(hdfsPattern, hdfsServer);
   server.post(hdfsPattern, hdfsServer);
   server.get(mapredPattern, mapredServer);
